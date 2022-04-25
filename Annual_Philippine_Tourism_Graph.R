cd4a8e1157de13ff625407a623e47142b8c6e105
Sheena Ondoy <sheenaondoy@Sheenas-Air.lan>
2022-04-25 13:48:14 -0500

    Graph Codes

--- Annual_Philippine_Tourism_Graph.R
+++ Annual_Philippine_Tourism_Graph.R
@@ -40,4 +40,34 @@ fig <- fig %>%
 
 fig
 
+#Expenditures----
+expenditures <- read_csv("Expenditures2.csv")
 
+fig2 <- plot_ly(data, x = ~Year, y = `Accommodation Services`, name = "South Korea", type = 'scatter', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~USA, name = 'USA', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~Japan, name = 'Japan', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~China, name = 'China', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~Taiwan, name = 'Taiwan', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~Australia, name = 'Australia', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~Singapore, name = 'Singapore', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~Canada, name = 'Canada', mode = 'lines+markers')%>%
+  add_trace(x = ~Year, y = ~UK, name = 'UK', mode = 'lines+markers' )%>%
+  add_trace(x = ~Year, y = ~Malaysia, name = 'Malaysia', mode = 'lines+markers')%>%
+  layout(title = 'Annual Tourists in Philippines by Country',
+         legend=list(title=list(text='variable')),
+         xaxis = list(dtick = "M1", tickformat="%b<br>%Y"),
+         yaxis = list(title = "Number of Tourists"),
+         width = 1000)
+options(warn = -1)
+fig2 <- fig2 %>%
+  layout(
+    xaxis = list(zerolinecolor = '#ffff',
+                 zerolinewidth = 2,
+                 gridcolor = 'ffff'),
+    yaxis = list(zerolinecolor = '#ffff',
+                 zerolinewidth = 2,
+                 gridcolor = 'ffff'),
+    plot_bgcolor='#e5ecf6')
+
+
+fig2
